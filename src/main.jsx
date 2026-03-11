32950b5cb28ba982d1e6a3c515957ae9c87ed456
Gregory <joystickgrindhouse@gmail.com>
2026-03-10 17:27:36 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
 import "../assets/styles/main.css";
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
